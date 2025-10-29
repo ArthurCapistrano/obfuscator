1b8bf23aaa60786c56f19f4b3e85fb3deaa792fb
arthurCapistrano <arthur.capistrano12@gmail.com>
2025-10-29 12:10:21 -0300

    add: initial setup to load data and create mysqldb

---
+++ src/database/setup.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from dotenv import load_dotenv
+from database.connection import DatabaseConnection
+from sqlalchemy.sql import text 
+import os
+
+# Load environment variables 
+load_dotenv(".env")
+
+def setup_database(db_conn: DatabaseConnection):
+    print(f"Verificando/Criando banco de dados: '{db_conn.db}'...")
+    
+    with db_conn.connect(engine= db_conn.get_server_engine()) as conn:
+        conn.execute(text(f"CREATE DATABASE IF NOT EXISTS {db_conn.db};"))
+    
+    print(f"Banco de dados '{db_conn.db}' verificado/criado com sucesso.")
+
+def load_origin_data(db_conn: DatabaseConnection):
+    csv_path = "data/raw/recruitment_data.csv"
+    
+    try:
+        df = pd.read_csv(csv_path)
+        print("Dados de origem carregados com sucesso.")
+    except Exception as e:
+        print(f"Erro ao carregar dados de origem: {e}")
+        return
+    
+    with db_conn.connect() as conn:
+        try:
+            df.to_sql('recruitment_data', 
+                      con=conn, 
+                      if_exists='replace', 
+                      index=False)
+            print("Dados de origem inseridos na tabela 'recruitment_data' com sucesso.")
+        except Exception as e:
+            print(f"Erro ao inserir dados na tabela 'recruitment_data': {e}")
+
+def main():
+    # Setup Origin Database 
+    origin_db = DatabaseConnection(prefix="ORIGIN")
+    setup_database(origin_db)
+    
+    # Load Origin Data
+    load_origin_data(origin_db)
+
+
+if __name__ == "__main__":
+    main()
